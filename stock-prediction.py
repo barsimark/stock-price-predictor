c30231045ec1a54f43802c0afdcdd468e831939f
barsimark <barsimark@gmail.com>
2022-03-01 16:03:55 +0100

    separate input data to train and test

--- stock-prediction.py
+++ stock-prediction.py
@@ -14,17 +14,30 @@ def load_dataset(dir: str) -> pd.DataFrame:
     df = pd.concat(df)
     df['Date'] = pd.to_datetime(df.Date)
     df.sort_values(by='Date', inplace=True)
-    print(df.head())
-    print("Length: ", len(df))
     return df
 
-def show_dataframe(df: pd.DataFrame):
+def show_dataframe_info(df: pd.DataFrame, title: str):
+    print("Title:", title)
+    print(df.head())
+    print("Length: ", len(df))
+
+def show_dataframe_chart(df: pd.DataFrame, title: str):
     plt.figure(figsize = (9,5))
     plt.plot(range(df.shape[0]),(df['Open']))
     plt.xlabel("Days", fontsize=18)
     plt.ylabel("Open price", fontsize=18)
+    plt.title(title)
     plt.show()
+
+def separate_data(df: pd.DataFrame):
+    last = df.tail(50)
+    df.drop(df.tail(50).index, inplace=True)
+    return df, last
     
 
 input = load_dataset("input")
-show_dataframe(input)
+x_train, x_test = separate_data(input)
+show_dataframe_info(x_train, "Train data")
+show_dataframe_info(x_test, "Test data")
+show_dataframe_chart(x_train, "Train data")
+show_dataframe_chart(x_test, "Test data")
